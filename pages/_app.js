678095389153bf446298f6c2c553d0af613019ff
MinjonH <97527244+MinjonH@users.noreply.github.com>
2022-08-14 09:49:11 +0200

    fixes GA

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,18 @@ import Navbar from '../components/Navbar';
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<head>
+				{/* Google Analytics */}
+				<script
+					async
+					src='https://www.googletagmanager.com/gtag/js?id=G-5HC8CCLQDN'
+				></script>
+				<script>
+					window.dataLayer = window.dataLayer || []; function gtag()
+					{dataLayer.push(arguments)}
+					gtag('js', new Date()); gtag('config', 'G-5HC8CCLQDN');
+				</script>
+			</head>
 			<Navbar />
 			<Component {...pageProps} />;
 		</>
