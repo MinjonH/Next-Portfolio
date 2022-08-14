4f17c5eb018c67a7ff10e9ee1c7321fb24d9cd14
MinjonH <97527244+MinjonH@users.noreply.github.com>
2022-08-14 09:57:08 +0200

    fix google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,24 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<head>
-				{/* Google Analytics */}
-				<script
-					async
-					src='https://www.googletagmanager.com/gtag/js?id=G-5HC8CCLQDN'
-				></script>
-				<script>
-					window.dataLayer = window.dataLayer || []; function gtag()
-					{dataLayer.push(arguments)}
-					gtag('js', new Date()); gtag('config', 'G-5HC8CCLQDN');
-				</script>
-			</head>
+			<Script
+				strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			<Script strategy='lazyOnload'>
+				{`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+			</Script>
 			<Navbar />
 			<Component {...pageProps} />;
 		</>
